3b8132144806bdeaee75dfc1016626cb237841bc
zhangchengcai <zhangchengcai3615@126.com>
2023-09-12 23:30:54 +0800

    Alamofire
    初始化项目

---
+++ Alamofire/Alamofire/Alamofire.h
@@ -0,0 +1,18 @@
+//
+//  Alamofire.h
+//  Alamofire
+//
+//  Created by zhangchengcai on 2023/9/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Alamofire.
+FOUNDATION_EXPORT double AlamofireVersionNumber;
+
+//! Project version string for Alamofire.
+FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alamofire/PublicHeader.h>
+
+
